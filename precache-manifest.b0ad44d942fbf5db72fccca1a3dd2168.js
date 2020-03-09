a38f32983eb9a14b5db2e1ae0fc26d73793ce3a3
Giselle Trithip <giselletrithip@gmail.com>
2020-03-09 21:08:21 +0700

    new

---
+++ precache-manifest.b0ad44d942fbf5db72fccca1a3dd2168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86b990d9c4a2443083c2a00fb940b6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5180be935be3599e14d1",
+    "url": "/static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "11d63afd9749789e7117",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5180be935be3599e14d1",
+    "url": "/static/js/2.40f155ff.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.40f155ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d63afd9749789e7117",
+    "url": "/static/js/main.dea9fd47.chunk.js"
+  },
+  {
+    "revision": "3013aafd85e2731fcd39",
+    "url": "/static/js/runtime-main.de95c6b4.js"
+  }
+]);
